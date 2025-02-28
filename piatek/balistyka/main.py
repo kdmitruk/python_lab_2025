b94482f0a1d6d0e966dd1253f8ead72bd07452d7
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-02-28 11:36:45 +0100

    krok 5

--- piatek/balistyka/main.py
+++ piatek/balistyka/main.py
@@ -1,15 +1,23 @@
-#import math
 from math import sin, radians, cos
 GRAVITY = 9.81
-angle = None
-# if not (angle>=0 and angle<=90):
-#     print("Błędny kąt")
-while angle is None or not(angle>=0 and angle<=90):
-    angle = float(input("Podaj kąt: "))
-velocity = float(input("Podaj prędkość: "))
-#print(angle)
-angle = radians(angle)
-print(velocity)
+def get_input():
+    angle = None
+    while angle is None or not(angle>=0 and angle<=90):
+        angle = float(input("Podaj kąt: "))
+    velocity = float(input("Podaj prędkość: "))
+    angle = radians(angle)
+    return angle, velocity
 
-z = velocity**2 * sin(2 * angle) / GRAVITY
-print(z)
+def calculate_impact(angle, velocity):
+    return velocity**2 * sin(2 * angle) / GRAVITY
+
+
+def main():
+    while True:
+        angle, velocity = get_input()
+        print(calculate_impact(angle, velocity))
+
+
+if __name__ == '__main__':
+    main()
+
