a5631f68ddafd8bf08d4892998ed107edd3ba724
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-02-28 11:10:18 +0100

    krok 3

--- piatek/balistyka/main.py
+++ piatek/balistyka/main.py
@@ -1,6 +1,8 @@
-angle = float(input("Podaj kąt: "))
-if not (angle>=0 and angle<=90):
-    print("Błędny kąt")
+angle = None
+# if not (angle>=0 and angle<=90):
+#     print("Błędny kąt")
+while angle is None or not(angle>=0 and angle<=90):
+    angle = float(input("Podaj kąt: "))
 velocity = float(input("Podaj prędkość: "))
 print(angle)
 print(velocity)
