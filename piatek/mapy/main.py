adb21448ee5b5b8ccb83913862010e8461c7cc31
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-03-07 11:13:46 +0100

    lab 2 zad 2

--- piatek/mapy/main.py
+++ piatek/mapy/main.py
@@ -1,11 +1,20 @@
 import curses
 
+PANEL_HEIGHT = 10
+
+def draw_separator(stdscr,height,width):
+    separator_y = height - PANEL_HEIGHT - 1
+    stdscr.addstr(separator_y, 0, '-' * width)
+
+
 def show_title_screen(stdscr,height,width):
     contents=["Map maker", "version 1.0", "", "Naciśnij dowolny klawisz aby kontynuować"]
     y=(height-len(contents))//2
     for i in range(len(contents)):
         x=(width-len(contents[i]))//2
         stdscr.addstr(y+i, x, contents[i])
+
+    stdscr.getch()
 
 def main(stdscr):
     curses.curs_set(0)
@@ -14,6 +23,8 @@ def main(stdscr):
     height, width = stdscr.getmaxyx()
     stdscr.clear()
     show_title_screen(stdscr,height,width)
+    stdscr.clear()
+    draw_separator(stdscr, height, width)
     stdscr.refresh()
     stdscr.getch()
 
