798e59b5ad9a7f04ac5944bbc4748e7c2cd49fdb
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-03-11 12:16:34 +0100

    lab 3 init

--- sroda/mapy/main.py
+++ sroda/mapy/main.py
@@ -1,13 +1,5 @@
 import curses
 
-PANEL_HEIGHT = 10
-
-def draw_separator(stdscr, rows, cols):
-    y = rows - (PANEL_HEIGHT + 1)
-    # stdscr.hline(y, 0, '-', cols)
-    stdscr.addstr(y, 0, '-' * cols)
-    stdscr.refresh()
-
 def show_title_screen(stdscr, rows, cols):
     contents = ["Map maker", "version 1.0", "", "Naciśnij dowolny klawisz aby kontynuować"]
     offset_y = (rows - len(contents)) // 2
@@ -19,16 +11,9 @@ def show_title_screen(stdscr, rows, cols):
     stdscr.getch()
 
 def draw_map(stdscr, rows, cols, structures):
-    # for structure in structures:
-    #     y,x = structure
-    #     stdscr.addstr(y,x,'*')
-    #     #stdscr.addstr(structure[0], structure[1],'*')
     global barracks
-    # for i, line in enumerate(barracks[1]):
-    #     stdscr.addstr(i, 0, line)
     for y, x in structures:
-        # stdscr.addstr(y,x,'*')
-        draw_structure(stdscr,x,y,barracks,centered = True, labeled = True, highlighted=True)
+        draw_structure(stdscr,x,y,barracks,centered = True)
 
 def draw_structure(stdscr,x,y,structure,centered = False, labeled = False, highlighted = False):
     name, art = structure
@@ -40,9 +25,7 @@ def draw_structure(stdscr,x,y,structure,centered = False, labeled = False, highl
         stdscr.addstr(offset_y + len(art)+1,offset_x,name,curses.color_pair(1 if highlighted else 0))
 
 def add_structure(structures, y, x, rows):
-    max_y = rows - (PANEL_HEIGHT + 1)
-    if y < max_y:
-        structures.append((y, x))
+    structures.append((y, x))
 
 def load_structure_from_file(path):
     with open(path) as fd:
@@ -55,18 +38,17 @@ barracks = load_structure_from_file('structures/barracks.txt')
 def main(stdscr):
     curses.start_color()
     curses.init_pair(1,curses.COLOR_YELLOW, curses.COLOR_BLACK)
+
     curses.curs_set(0)
     curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
 
     height, width = stdscr.getmaxyx()
     stdscr.clear()
-    # stdscr.addstr(5, 10, f"* {height}x{width} *")
     show_title_screen(stdscr, height, width)
     stdscr.clear()
     structures = []
     while True:
         draw_map(stdscr, height, width, structures)
-        draw_separator(stdscr, height, width)
         stdscr.refresh()
 
         key = stdscr.getch()
