976c2e4d6632c84e49d2480a45e004f45523d3ff
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-03-14 12:51:21 +0100

    lab3 krok 5, 6

--- piatek/mapy/main.py
+++ piatek/mapy/main.py
@@ -35,13 +35,13 @@ def draw_structure(stdscr, structure, x, y, centered = False, labeled = False, h
 def add_structure(structures, y, x,height, available_structures, active_structure):
     structures.append((y,x, list(available_structures.keys())[active_structure]))
 
-def draw_map(stdscr, structures, available_structures):
+def draw_map(stdscr, structures, available_structures,offset_x,offset_y):
     for y, x, name in structures:
-        draw_structure(stdscr,available_structures[name],x,y,centered=True)
+        draw_structure(stdscr,available_structures[name],x-offset_x,y-offset_y,centered=True)
 
-def draw_scene(stdscr, structures, active_structure, available_structures, height, width):
+def draw_scene(stdscr, structures, active_structure, available_structures, height, width,offset_x,offset_y):
     stdscr.clear()
-    draw_map(stdscr, structures, available_structures)
+    draw_map(stdscr, structures, available_structures,offset_x,offset_y)
     stdscr.addstr(height - 1, 0, list(available_structures.keys())[active_structure])
     stdscr.refresh()
 
@@ -56,6 +56,7 @@ def load_map(file_path):
     structures = pickle.load(file)
     file.close()
     return structures
+
 
 def show_title_screen(stdscr,height,width):
     contents=["Map maker", "version 1.0", "", "Naciśnij dowolny klawisz aby kontynuować"]
@@ -74,6 +75,8 @@ def main(stdscr):
     curses.init_pair(1,curses.COLOR_YELLOW,curses.COLOR_BLACK)
     curses.curs_set(0)
     curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
+    offset_x = 0
+    offset_y = 0
 
     if len(sys.argv) == 2:
         structures = load_map(sys.argv[1])
@@ -84,7 +87,7 @@ def main(stdscr):
     stdscr.clear()
     show_title_screen(stdscr,height,width)
     while True:
-        draw_scene(stdscr, structures, active_structure, available_structures, height, width)
+        draw_scene(stdscr, structures, active_structure, available_structures, height, width,offset_x,offset_y)
         ch = stdscr.getch()
         if ch == ord('q'):
             break
@@ -95,7 +98,15 @@ def main(stdscr):
         elif ch == curses.KEY_MOUSE:
             _, x, y, _, bstate = curses.getmouse()
             if bstate & curses.BUTTON1_CLICKED:
-                add_structure(structures, y, x,height, available_structures, active_structure)
+                add_structure(structures, y+offset_y, x+offset_x,height, available_structures, active_structure)
+        elif ch == curses.KEY_LEFT:
+            offset_x -=1
+        elif ch == curses.KEY_RIGHT:
+            offset_x +=1
+        elif ch == curses.KEY_UP:
+            offset_y -=1
+        elif ch == curses.KEY_DOWN:
+            offset_y +=1
 
 
 if __name__ == '__main__':
