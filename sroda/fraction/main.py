e9db519eedd7c42c57e292a4526ccee4f9327b39
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-03-18 12:07:41 +0100

    lab4 zad 1c

--- sroda/fraction/main.py
+++ sroda/fraction/main.py
@@ -1,18 +1,21 @@
 class Fraction:
-    nominator = None
-    denominator = None
+    def __init__(self, nominator=None, denominator=None):
+        self.nominator = nominator
+        self.denominator = denominator
 
+    def is_integer(self):
+        return self.nominator % self.denominator == 0
 
 
 def main():
     f = Fraction()
-    f.nominator = 3
-    f.denominator = 7
+    f.nominator = 6
+    f.denominator = 6
     nf = Fraction()
     nf.nominator = 5
     nf.denominator = 9
-    print(f"{f.nominator}/{f.denominator}")
-    print(f"{nf.nominator}/{nf.denominator}")
+    print(f"{f.nominator}/{f.denominator} czy integer: ", f.is_integer())
+    print(f"{nf.nominator}/{nf.denominator} czy integer: ", nf.is_integer())
 
 
 if __name__ == '__main__':
