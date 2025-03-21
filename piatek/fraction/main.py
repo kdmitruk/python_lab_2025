d5104a9e94538f49631c4ba07aa5d7fc6cfdb402
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-03-21 10:54:35 +0100

    lab4 zad 1b

--- piatek/fraction/main.py
+++ piatek/fraction/main.py
@@ -2,15 +2,19 @@ class Fraction:
     nominator = None
     denominator = None
 
+    def is_integer(self):
+        return self.nominator % self.denominator == 0
+
 def main():
     fraction = Fraction()
     fraction2 = Fraction()
-    fraction.nominator = 1
+    fraction.nominator = 2
     fraction.denominator = 2
     fraction2.nominator = 3
     fraction2.denominator = 4
     print(f"{fraction.nominator}/{fraction.denominator}")
     print(f"{fraction2.nominator}/{fraction2.denominator}")
+    print(fraction.is_integer())
 
 if __name__ == '__main__':
     main()
