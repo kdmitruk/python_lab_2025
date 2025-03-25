eee2fd5d5dfe26cda67159aed4d16afe90f6f2d9
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-03-25 12:17:34 +0100

    lab 5 zad 1

---
+++ sroda/weather/main.py
@@ -0,0 +1,11 @@
+from PySide6.QtWidgets import QApplication, QWidget
+
+
+def main():
+    app = QApplication()
+    widget = QWidget()
+    widget.show()
+    return app.exec()
+
+if __name__ == '__main__':
+    main()
