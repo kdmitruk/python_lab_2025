c128cd236ec8c7a310bc2f613222282de2ee3e75
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-03-25 12:49:44 +0100

    lab 5 zad 5

--- sroda/weather/main_widget.py
+++ sroda/weather/main_widget.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QGridLayout
-
+import requests
 
 class MainWidget(QWidget):
     def __init__(self):
@@ -8,7 +8,7 @@ class MainWidget(QWidget):
 
         self.label = QLabel(self)
         self.button = QPushButton(self)
-        self.edit = QLineEdit(self)
+        self.edit = QLineEdit("Lublin", self)
 
         self.button.clicked.connect(self._on_button_clicked)
 
@@ -19,7 +19,9 @@ class MainWidget(QWidget):
 
     def _on_button_clicked(self):
         text = self.edit.text()
-        self.label.setText(text)
+        #self.label.setText(text)
+        response = requests.get(f'https://geocoding-api.open-meteo.com/v1/search?name={text}')
+        print(response.json())
 
 
 
