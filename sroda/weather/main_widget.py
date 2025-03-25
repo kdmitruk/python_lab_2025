e25826c402eb3a8e5d8a065b26c5b849d02f36f0
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-03-25 12:31:42 +0100

    lab 5 zad 2

--- sroda/weather/main_widget.py
+++ sroda/weather/main_widget.py
@@ -1,7 +1,22 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QGridLayout
 
 
 class MainWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Weather")
+
+        self.label = QLabel(self)
+        self.button = QPushButton(self)
+        self.edit = QLineEdit(self)
+
+        layout = QGridLayout(self)
+        layout.addWidget(self.edit, 0, 0, 1, 1)
+        layout.addWidget(self.button, 0, 1, 1, 1)
+        layout.addWidget(self.label, 1, 0, 1, 2)
+
+
+
+
+
+
