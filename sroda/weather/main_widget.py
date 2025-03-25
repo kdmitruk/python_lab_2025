4693c7d08351084bb0a3f4f0df359d7b35de4a67
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-03-25 13:09:10 +0100

    lab 5 zad 6

--- sroda/weather/main_widget.py
+++ sroda/weather/main_widget.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QGridLayout
+from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QGridLayout, QMessageBox
 import requests
 
 class MainWidget(QWidget):
@@ -8,7 +8,7 @@ class MainWidget(QWidget):
 
         self.label = QLabel(self)
         self.button = QPushButton(self)
-        self.edit = QLineEdit("Lublin", self)
+        self.edit = QLineEdit("abdsabsd", self)
 
         self.button.clicked.connect(self._on_button_clicked)
 
@@ -22,6 +22,11 @@ class MainWidget(QWidget):
         #self.label.setText(text)
         response = requests.get(f'https://geocoding-api.open-meteo.com/v1/search?name={text}')
         #print(response.json())
+        json = response.json()
+        if "results" not in json.keys():
+            QMessageBox.information(self, "error", "Brak miejscowosci")
+            return
+
         results = response.json()["results"]
         #print(results)
         result = results[0]
