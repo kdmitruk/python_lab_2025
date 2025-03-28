d4809341ada5156287f745f3371d863653b5eb7f
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-03-28 10:50:04 +0100

    lab 5 zad 1

---
+++ piatek/weather/main.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QApplication, QWidget
+
+def main():
+    app = QApplication()
+    widget = QWidget()
+    widget.show()
+    return app.exec()
+
+if __name__ == '__main__':
+    main()
