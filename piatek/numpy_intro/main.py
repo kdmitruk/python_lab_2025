d00fdb545c232bda78f53adb5acaf2e528dc735b
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-05-09 12:00:16 +0200

    lab9 zad4

--- piatek/numpy_intro/main.py
+++ piatek/numpy_intro/main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+SIZE = (100, 100)
+
 def ex1():
     array_1d = np.array([1,2,3,4,5], dtype=np.uint8)
     array_2d = np.array([[1,2,3],[4,5,6]])
@@ -9,15 +11,25 @@ def ex1():
     print(f'array_2d: {array_2d}, shape: {array_2d.shape}, dtype: {array_2d.dtype}')
 
 def ex2():
-    img = np.zeros((100, 100), dtype=np.uint8)
+    img = np.zeros(SIZE, dtype=np.uint8)
     img[35, 77] = 255
     plt.imshow(img, cmap='gray')
     plt.show()
 
 def ex3():
-    img = np.random.randint(0, 256, size=(100, 100))
+    img = np.random.randint(0, 256, size=SIZE)
     plt.imshow(img, cmap='gray')
     plt.show()
 
+def ex4(show = False):
+    img_normal = np.random.normal(loc = 127, scale=1, size = SIZE)
+    img_uniform =  np.random.randint(0, 256, size=SIZE)
+    _,(ax_normal, ax_uniform) = plt.subplots(1, 2)
+    if show:
+        ax_normal.imshow(img_normal)
+        ax_uniform.imshow(img_uniform)
+        plt.show()
+    return img_uniform, img_normal
+
 if __name__ == '__main__':
-    ex3()
+    ex4(True)
