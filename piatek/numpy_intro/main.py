98f57b5b07cac08a9328833fb811beb9ad0c0245
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-05-09 12:23:52 +0200

    lab9 zad5

--- piatek/numpy_intro/main.py
+++ piatek/numpy_intro/main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-SIZE = (100, 100)
+SIZE = (5, 5)
+IMSHOW_ARGS = {"vmin": 0, "vmax": 255, "cmap": 'gray'}
 
 def ex1():
     array_1d = np.array([1,2,3,4,5], dtype=np.uint8)
@@ -23,13 +24,22 @@ def ex3():
 
 def ex4(show = False):
     img_normal = np.random.normal(loc = 127, scale=1, size = SIZE)
-    img_uniform =  np.random.randint(0, 256, size=SIZE)
-    _,(ax_normal, ax_uniform) = plt.subplots(1, 2)
+    img_uniform =  np.random.randint(0, 256, size=SIZE,dtype=np.uint8)
     if show:
+        _,(ax_normal, ax_uniform) = plt.subplots(1, 2)
         ax_normal.imshow(img_normal)
         ax_uniform.imshow(img_uniform)
         plt.show()
     return img_uniform, img_normal
 
+def ex5(img,brightness,contrast):
+    img = np.clip(img.copy().astype(np.float32)*contrast + brightness,0,255).astype(np.uint8)
+    return img
+
+
 if __name__ == '__main__':
-    ex4(True)
+    img,_ = ex4(False)
+    _,axes = plt.subplots(1,2)
+    axes[0].imshow(img, **IMSHOW_ARGS)
+    axes[1].imshow(ex5(img,100,1.2), **IMSHOW_ARGS)
+    plt.show()
