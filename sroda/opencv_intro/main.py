db14f68aa7284efcc7dda3122f6e17ea3db601d4
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-05-13 13:27:24 +0200

    lab10 zad4

--- sroda/opencv_intro/main.py
+++ sroda/opencv_intro/main.py
@@ -1,5 +1,36 @@
 import cv2
 
+
+class Viewer:
+    def __init__(self, slider_name, current_value, end_value):
+        self.slider_name = slider_name
+        self.start_value = current_value
+        self.end_value = end_value
+        self.cam = cv2.VideoCapture(0)
+        if self.cam is not None and self.cam.isOpened():
+            cv2.namedWindow("okno")
+            cv2.createTrackbar(slider_name, "okno", current_value, end_value, lambda x:x)
+
+    def get_slider_value(self):
+        return cv2.getTrackbarPos(self.slider_name, "okno")
+
+    def process_frame(self, frame, value):
+        return frame
+
+    def run(self):
+        value = 0
+        while True:
+            ok, frame = self.cam.read()
+            if not ok:
+                break
+            cv2.flip(frame, 1, frame)
+            value = self.get_slider_value()
+            # frame = cv2.add(frame, value)
+            frame = self.process_frame(frame,value)
+            cv2.imshow("okno", frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
 def ex1():
     img = cv2.imread("grzybek.png")
     cv2.imshow("okno", img)
@@ -24,7 +55,8 @@ def ex2():
 
 
 def main():
-    ex2()
+    viewer = Viewer("slider",0,255)
+    viewer.run()
 
 
 if __name__ == '__main__':
