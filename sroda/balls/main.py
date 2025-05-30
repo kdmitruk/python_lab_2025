e659de6ab7f9d79fc42dbe281ed5a39a90b10f92
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-05-30 12:09:04 +0200

    lab12 z2

--- sroda/balls/main.py
+++ sroda/balls/main.py
@@ -1,7 +1,7 @@
 import random
 
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import AmbientLight, DirectionalLight, LVector3, LineSegs, Vec3
+from panda3d.core import AmbientLight, DirectionalLight, LVector3, LineSegs, Vec3, Point2
 
 TABLE_SIZE = 10
 
@@ -9,26 +9,27 @@ class Ball:
     def __init__(self,pos,color,render,loader):
         self.pos = pos
         self.color = color
-        node = loader.loadModel("models/sphere")
-        node.setScale(0.1)
-        node.setPos(self.pos)
-        node.setColor(self.color)
-        node.reparentTo(render)
+        self.node = loader.loadModel("models/sphere")
+        self.node.setScale(0.1)
+        self.node.setPos(self.pos)
+        self.node.setColor(self.color)
+        self.node.reparentTo(render)
 
 
 class Game(ShowBase):
     def __init__(self):
         super().__init__()
         self.disable_mouse()
-
         self.camera.set_pos(0, -15, 12)
         self.camera.look_at(0, 0, 0)
 
+        self.accept('mouse1',self.on_mouse_pressed)
+
         randpos = lambda: random.random()*TABLE_SIZE-TABLE_SIZE/2
-        balls = []
-        balls.append(Ball(Vec3(randpos(),randpos(),0),(1,0,0,1),self.render,self.loader))
-        balls.append(Ball(Vec3(randpos(),randpos(),0),(0,1,0,1),self.render,self.loader))
-        balls.append(Ball(Vec3(randpos(),randpos(),0),(0,0,1,1),self.render,self.loader))
+        self.balls = []
+        self.balls.append(Ball(Vec3(randpos(),randpos(),0),(1,0,0,1),self.render,self.loader))
+        self.balls.append(Ball(Vec3(randpos(),randpos(),0),(0,1,0,1),self.render,self.loader))
+        self.balls.append(Ball(Vec3(randpos(),randpos(),0),(0,0,1,1),self.render,self.loader))
 
         ambient = AmbientLight('ambient')
         ambient.set_color((0.5, 0.5, 0.5, 1))
@@ -50,6 +51,19 @@ class Game(ShowBase):
         ls.set_thickness(3)
         node = ls.create()
         self.render.attach_new_node(node)
+
+    def on_mouse_pressed(self):
+        mouse = self.mouseWatcherNode.getMouse()
+        # print(mouse)
+        for ball in self.balls:
+            node = ball.node
+            point = node.getPos(self.cam)
+            projected_point = Point2()
+            # print(lens)
+            # print(point)
+            if self.camLens.project(point,projected_point):
+                print(projected_point, mouse)
+
 
 if __name__ == '__main__':
     app = Game()
