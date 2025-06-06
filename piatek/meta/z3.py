f895264094c51d0b8cd5628b09e3ea8bf066b972
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-06-06 11:21:02 +0200

    lab 13 zad 3

---
+++ piatek/meta/z3.py
@@ -0,0 +1,18 @@
+class RegistryClass(type):
+    registry = {}
+
+    def __new__(cls, name, base, fields):
+        created_class = super().__new__(cls,name,base,fields)
+        RegistryClass.registry[name] = created_class
+        return created_class
+
+class C1(metaclass=RegistryClass):
+    def __init__(self):
+        self.name = "C1"
+
+def main():
+    c1 = C1()
+    print(RegistryClass.registry)
+
+if __name__ == '__main__':
+    main()
